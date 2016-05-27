a10916088fd2ae7224fa88c98b1aaba0c9bcebb3
banacl <rekil896@gmail.com>
2016-05-27 23:37:42 +0530

    lec 58 rotation up and down
    first person camera feeling

--- OpenGl1/Camera.cpp
+++ OpenGl1/Camera.cpp
@@ -7,8 +7,15 @@ UP(0.0f,1.0f,0.0f)//need not be nomalized
 }
 void Camera::mouseUpdate(const glm::vec2& newMousePosition)
 {
+	//in X - plane  ie around UP vector
 	glm::vec2 mouseDelta = newMousePosition - oldMousePosition;
 	viewDirection = glm::mat3(glm::rotate(mouseDelta.x/25.0f, UP))*viewDirection;
+	
+	//up-down in the Y plane
+	glm::vec3 newRotationAxis = glm::cross(viewDirection,UP);
+	viewDirection = glm::mat3(glm::rotate(mouseDelta.y / 25.0f, newRotationAxis))*viewDirection;
+
+	
 	oldMousePosition = newMousePosition;
 }
 glm::mat4 Camera::getWorldToViewMatrix() const
